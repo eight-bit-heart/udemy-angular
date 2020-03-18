69e69c2685bbd547f72287f435e77f87d8e98a12
Paul Miller <Pmiller@DTIS.COM>
2020-03-18 08:46:22 -0400

    Incremental updates from 03-17-20

---
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Spaghetti', 'Delicious Pasta', 'https://storage.needpix.com/rsynced_images/spaghetti-2696723_1280.jpg'),
+    new Recipe('Sushi', 'Yummy rice and raw fishes!', 'https://cdn.pixabay.com/photo/2017/03/02/20/53/sushi-2112350_960_720.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
